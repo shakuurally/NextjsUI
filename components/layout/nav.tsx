c1120451ec2b0b1bfcf9db98f4eadadb3ed7986d
shakuurally <abdishakuuralimohamed@gmail.com>
2023-09-17 09:57:20 +0300

    setup google_next auth

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Nav() {
+
   const session = await getServerSession(authOptions);
   return <Navbar session={session} />;
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
   },
+
   async redirects() {
     return [
       {
